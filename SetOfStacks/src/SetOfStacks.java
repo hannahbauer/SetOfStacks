a31204ac000515c12c3dc5d4e9b34b6636ca3eff
Hannah Bauer <hbauer2021@chadwickschool.org>
2018-10-31 11:49:13 -0700

    feat: Add new SetOfStacks class with all methods except for popAt

--- SetOfStacks/src/SetOfStacks.java
+++ SetOfStacks/src/SetOfStacks.java
@@ -3,56 +3,47 @@ public class SetOfStacks {
     static int capacity = 100;//capacity for each stack
     int[] array = new int [300];
     
-    static int stack0 = -1;
-    static int stack1 = -1;
-    static int stack2 = -2;
-    int stack;
+    int numInStack;
     
-    public static void main(String[] args) {
-        int stack = stack0;
-        if (capacity > 100) {
-            stack = stack1;
-            }
+    public SetOfStacks() {
+        numInStack = 0;
+        array = new int[capacity];
         
-        }
+    }
     
-    private int lastStack(int stackNumber) {
-        switch (stackNumber) {
-        case 0:
-            return stackNumber * capacity + stack0;
-        case 1:
-            return stackNumber * capacity + stack1;
-        case 2:
-            return stackNumber * capacity + stack2;
-        } 
-        throw new Error("Unexpected stack num.");
-        }//test
-            
-    public int push (int stackNumber, int element) {
-        array[lastStack(stackNumber) + 1] = element;
-        
-        switch (stackNumber) {
-        case 0:
-            return stack0 + 1;      
-        case 1:
-            return stack1 + 1;
-        case 2:
-            return stack2 + 1;
-            
-        default:
-            throw new Error("Unexpected stack num.");
-        }
+    public SetOfStacks(int beginCap) {
+        numInStack = 0;
+        array = new int[beginCap];
     }
-    public int pop(int stackNumber) {
-        switch(stackNumber) {
-        case 0:
-            return stack0 - 1;
-        case 1:
-            return stack1 - 1;
-        case 2:
-            return stack2 - 1;
-            default:
-                throw new Error("Unexpected stack num");
+    
+    protected void moveStack() {
+        int[] newStack = new int[array.length * 2];
+        for (int i = 0; i < array.length; i++) {
+            newStack[i] = array[i];
         }
+        array = newStack;
     }
+    
+    public void push(int element) {
+        if (array.length == capacity) {
+            moveStack();
+        }
+        array[numInStack] = element;
+        numInStack++;
+    }
+    
+    public int pop() throws Error {
+        if (array.length == 0) {
+            throw new Error("Next stack is empty");
+        }
+        numInStack--;
+        int result = array[numInStack];
+        array[numInStack] = 0;
+        return result;
+    }
+    
+//    public void popAt(int StackNumber) {
+//        StackNumber.capacity;
+//    }
+    
 }
